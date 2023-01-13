be560da262762b087a1ae53e8c69274aaa022e4a
+ <stingeyandrewstown@live.com>
2023-01-13 15:53:57 -0600

    commit tree 0eb13c24c3d26c3ebf51bbf2d1785e9afa4bf367

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your code here
+const { use } = require("chai")
+
+function submitData(name, email){
+    console.log(JSON.stringify({name: name, email: email}))
+    fetch('http://localhost:3000/users',{
+        method: 'POST',
+        header: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({name: name, email: email})
+    })
+    .then(res => res.json())
+    .then(user => console.log(user))
+}
